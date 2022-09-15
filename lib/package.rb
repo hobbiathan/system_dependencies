0b9d0a5f11e79122c3e2c3ba0f5795d5e89a0672
hobbiathan <hobbiathan@gmail.com>
2022-09-14 19:04:15 -0600

    install/uninstall/install_status methods

--- lib/package.rb
+++ lib/package.rb
@@ -1,8 +1,19 @@
 class Package
-  attr_reader :name, :dependencies
+  attr_reader :name, :dependencies, :install_status
 
   def initialize(name, dependencies)
     @name = name
     @dependencies = dependencies
+    @install_status = false
+  end 
+
+  def install
+    puts "Installing #{@name}."
+    @install_status = true
+  end 
+
+  def uninstall
+    puts "Uninstalling #{@name}."
+    @install_status = false
   end 
 end 
