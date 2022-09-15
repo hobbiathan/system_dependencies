678f9247a459994bd7ef468f8ed0d4d937a6364e
hobbiathan <hobbiathan@gmail.com>
2022-09-14 18:10:02 -0600

    fix scope

--- spec/dependency_spec.rb
+++ spec/dependency_spec.rb
@@ -1,18 +1,26 @@
 require 'rspec'
-require '../lib/dependency'
+require './lib/dependency'
 
 RSpec.describe Dependency do
   describe 'it exists' do
     before(:each) do 
       @dp = Dependency.new("TCPIP")
+    end  
+
+    it 'is of type Dependency' do
+      expect(@dp).to be_a Dependency
     end 
-  end 
 
-  it 'is of type Dependency' do
-    expect(@dp).to be_a Dependency
-  end 
+    it 'can read dependency name' do
+      expect(@dp.name).to eq("TCPIP")
+    end 
 
-  it 'can read dependency name' do
-    expect(@dp.name).to eq("TCPIP")
+    it 'has a default install status of false' do
+      expect(@dp.install_status).to be false
+    end 
+
+    it 'has default install count of 0' do
+      expect(@dp.install_count).to eq(0)
+    end
   end 
 end 
