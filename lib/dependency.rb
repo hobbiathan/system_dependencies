77370feedd237f5b83c98e7c0723bd60502fcc5f
hobbiathan <hobbiathan@gmail.com>
2022-09-14 18:27:42 -0600

    install/uninstall methods

--- lib/dependency.rb
+++ lib/dependency.rb
@@ -14,4 +14,18 @@ class Dependency
   def minusone
     @install_count -= 1
   end 
+
+  def install
+    puts "Installing #{@name}"
+    @install_status = true
+  end 
+
+  def uninstall
+    if @install_count > 0
+      puts "#{@name} is still needed."
+    else
+      puts "Removing #{@name}."
+      @install_status = false
+    end 
+  end 
 end 
